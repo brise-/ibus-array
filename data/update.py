31a77b27d008ade776bca7005edd62e804efbecc
brise- <flylieh@hotmail.com>
2016-08-16 20:58:39 +0800

    Update encoded table and database. (Source: www.array.com.tw)

--- data/update.py
+++ data/update.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from pysqlite2 import dbapi2 as sqlite
+import sqlite3
 
 def array_updatedb(table_file):
-	con = sqlite.connect("array.db")
+	con = sqlite3.connect("array.db")
 	cur = con.cursor()
 	cur.execute('select * from main')
 	main = cur.fetchall()
@@ -23,12 +23,13 @@ def array_updatedb(table_file):
 	con.close()
 
 # empty main table
-con = sqlite.connect("array.db")
+con = sqlite3.connect("array.db")
 cur = con.cursor()
 cur.execute('delete from main where keys not in ("w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8", "w9", "w0");')
 con.commit()
 con.close()
 
-array_updatedb('array30_27489.utf8')
-array_updatedb('array30_ExtB.utf8')
-array_updatedb('array30_ExtCD_V2012A.utf8')
+array_updatedb('array30_27489-V201509.utf8')
+array_updatedb('array30_ExtB_V2016A.utf8')
+array_updatedb('array30_ExtCD_V2013A.utf8')
+array_updatedb('array30_ExtE_V2016A.utf8')
